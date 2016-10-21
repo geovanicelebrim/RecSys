42573dbc34ac50f422e8af2ad78ffb542a533a2b
Cassiano <honoriocassiano@gmail.com>
2016-10-21 12:09:27 -0200

    Adição dos testes

--- RSVD/svd.py
+++ RSVD/svd.py
@@ -93,14 +93,17 @@ com os dados de teste
 @return test_mae Resultado do MAE da última época
 @return elapsed Tempo gasto para executar o modelo
 """
-def rsvd(I, rating_train, rating_test, filename, lamb=0.1, k=15, n_epochs=30, lrate=0.01):
+def rsvd(I, rating_train, rating_test, filename='./tests/svd_erros_teste.csv', lamb=0.1, k=15, n_epochs=30, lrate=0.01):
 	n_users, n_items = rating_train.shape  # Número de usuários e itens
 	P = np.random.rand(k,n_users)
 	Q = np.random.rand(k,n_items)
 	users,items = rating_train.nonzero()
 
-	output = open(filename, 'w')
-	output.write("epoch,rmse,mae\n")
+	best_test_rmse = float('inf')
+	best_test_mae = float('inf')
+
+	# output = open(filename, 'w')
+	# output.write("epoch,rmse,mae\n")
 	start = time.time()
 	for epoch in range(n_epochs):
 		print("\rProgresso: ", ((epoch+1)*100)//n_epochs, "%", end="")
@@ -109,13 +112,25 @@ def rsvd(I, rating_train, rating_test, filename, lamb=0.1, k=15, n_epochs=30, lr
 			P[:,u] += lrate * ( e * Q[:,i] - lamb * P[:,u])
 			Q[:,i] += lrate * ( e * P[:,u] - lamb * Q[:,i])
 		test_rmse, test_mae = calc_errors(I,rating_test,P,Q)
-		output.write("{},{},{}\n".format(epoch,test_rmse,test_mae))
-		output.flush()
-	output.close()
+
+		if test_rmse < best_test_rmse:
+			best_test_rmse = test_rmse
+
+		if test_mae < best_test_mae:
+			best_test_mae = test_mae
+		# output.write("{},{},{}\n".format(epoch,test_rmse,test_mae))
+		# output.flush()
+	# output.close()
 	print("")
 	elapsed = time.time() - start
-	return(test_rmse, test_mae, elapsed)
+	# return(test_rmse, test_mae, elapsed)
+	return(best_test_rmse, best_test_mae, elapsed)
 
+
+best_lamb = 0.1
+best_k = 30
+best_n_epochs = 100
+best_lrate = 0.01
 
 """
 Sugestão de parâmetros do paper "Improving regularized singular value decomposition for
@@ -129,5 +144,56 @@ lrate = (ideal: 0.01) Max(0.07) Min(0.005) Passo(0.0065)
 """
 if __name__ == '__main__':
 	I, rating_train, rating_test = crete_dataset()
-	rmse, mae, elapsed = rsvd(I, rating_train, rating_test, './svd_erros_teste.csv', lamb=0.05)
-	print("Último RMSE: {:.4f}, Último MAE: {:.4f}, Tempo gasto: {:.4f} sec.".format(rmse, mae, elapsed))
+
+	###########################################################################
+	# Variando o lambda
+	output = open("./tests/test_lambda.csv", "w")
+	output.write("lambda,rmse,mae\n")
+
+	for l in range(0.05, 0.15, 0.001):
+		print("Testando para lambda = ", l)
+		rmse, mae, elapsed = rsvd(I, rating_train, rating_test, lamb=l)
+		output.write("{},{},{}\n".format(l, rmse, mae))
+
+	output.close()
+
+	###########################################################################
+	# Variando o lrate
+	output = open("./tests/test_lrate.csv", "w")
+	output.write("lrate,rmse,mae\n")
+
+	for l in range(0.005, 0.07, 0.0065):
+		print("Testando para lrate = ", l)
+		rmse, mae, elapsed = rsvd(I, rating_train, rating_test, lrate=l)
+		output.write("{},{},{}\n".format(l, rmse, mae))
+
+	output.close()
+
+	###########################################################################
+	# Variando o k
+	output = open("./tests/test_k.csv", "w")
+	output.write("k,rmse,mae\n")
+
+	for k in range(10, 40, 5):
+		print("Testando para k = ", k)
+		rmse, mae, elapsed = rsvd(I, rating_train, rating_test, k=k)
+		output.write("{},{},{}\n".format(k, rmse, mae))
+
+	output.close()
+
+	###########################################################################
+	# Variando o epoch
+	output = open("./tests/test_epochs.csv", "w")
+	output.write("epoch,rmse,mae\n")
+
+	for epoch in range(10, 40, 5):
+		print("Testando para epoch = ", epoch)
+		rmse, mae, elapsed = rsvd(I, rating_train, rating_test, n_epochs=epoch)
+		output.write("{},{},{}\n".format(epoch, rmse, mae))
+
+	output.close()
+
+	###########################################################################
+
+	# rmse, mae, elapsed = rsvd(I, rating_train, rating_test, './svd_erros_teste.csv')
+	# print("Último RMSE: {:.4f}, Último MAE: {:.4f}, Tempo gasto: {:.4f} sec.".format(rmse, mae, elapsed))
