53451debf931477b135cef0a9f398c14e5cf4548
Cassiano Honorio <honoriocassiano@gmail.com>
2016-11-13 12:43:05 -0200

    Alteração das funções de teste

--- KNN/knnClassifier.py
+++ KNN/knnClassifier.py
@@ -1,6 +1,6 @@
 #-*-coding:utf-8-*-
 
-from cross_validation import treino_teste_split, calcular_mae, calcular_rmse, treino_teste_split2
+from cross_validation import calcular_mae, calcular_rmse, dividir_base
 from similarities import cosseno, cosseno_intersecao, pearson, sc_dice
 from scipy.sparse import lil_matrix as esparsa
 from numpy import nonzero, argsort, dot, zeros, count_nonzero, std
@@ -12,6 +12,24 @@ u_user = path + "u.user"
 u_item = path + "u.item"
 u_data = path + "u.data"
 
+dados_divididos = None
+
+def treino_teste_split(arquivo, parte, divisoes):
+
+	global dados_divididos
+
+	if dados_divididos == None:
+		dados_divididos = dividir_base(arquivo, divisoes)
+
+	# treino = dados_divididos[ : parte ]
+	# treino += dados_divididos[ parte+1: ]
+	treino = [ i for j in dados_divididos[ : parte ] for i in j]
+	treino += [ i for j in dados_divididos[ parte+1: ] for i in j]
+
+	teste = dados_divididos[parte]
+
+	return treino, teste
+
 """
 @brief Cria os dados que alimentam o knn
 
@@ -22,9 +40,10 @@ Dados os conjuntos de dados, gera os dados de treino e teste necessários para o
 @return matriz_teste Matriz esparsa (usuário-item) de teste
 @return dados_teste Dados do conjunto de teste
 """
-def criar_dataset():
+def criar_dataset(parte, divisoes):
 
-	treino, teste = treino_teste_split(u_data)
+	# treino, teste = treino_teste_split(u_data)
+	treino, teste = treino_teste_split(u_data, parte, divisoes)
 
 	with open(u_user, encoding="utf-8") as a:
 		usuarios = a.readlines()	
@@ -93,73 +112,62 @@ A função de similaridade e o número de vizinhos são dados como entrada.
 @param min_k Menor número de vizinhos mais próximos. Default 1
 @param max_k Maior número de vizinhos mais próximos. Default 5
 @param intervalo Taxa de acréscimo do número de vizinhos
-@param arquivo_saida Arquivo de saída contendo os dados da execução do algoritmo
+@param arquivo_saida Arquivo de saída contendo os dados da execução do algoritmo (sem a extensão)
 @return Média absoluta dos erros
 
 """
-def classificar(func_similaridade=cosseno, min_k=1, max_k=5, acrescimo=1, arquivo_saida="saida.txt", **parametros):
+def classificar(func_similaridade=cosseno, min_k=1, max_k=5, acrescimo=1, arquivo_saida="saida", divisoes=5, **parametros):
 
-	matriz_treino, dados_treino, matriz_teste, dados_teste = criar_dataset() 
+	for i in range(divisoes):
+		matriz_treino, dados_treino, matriz_teste, dados_teste = criar_dataset(i, divisoes)
 
-	similaridades = func_similaridade(matriz_treino, **parametros)
-	
-	gabarito = [dado[2] for dado in dados_teste]
+		print("Calculando para a parte ", i)
 
-	print("")
-
-	for k in range(min_k, max_k+1, acrescimo):
+		similaridades = func_similaridade(matriz_treino, **parametros)
 		
-		print("Calculando para k =", k)
+		gabarito = [dado[2] for dado in dados_teste]
 
-		predito = zeros(len(dados_teste))
+		print("")
 
-		for i in range(len(dados_teste)):
+		for k in range(min_k, max_k+1, acrescimo):
+			
+			print("Calculando para k =", k)
 
-			print("\rCalculando predições: ", ((i+1)*100)//len(dados_teste), "%", end="")
+			predito = zeros(len(dados_teste))
 
-			usuario, item = dados_teste[i][0], dados_teste[i][1]
-			vizinhos = nonzero(matriz_treino[:, item])[0] 
-			vizinhos = vizinhos[similaridades[usuario, vizinhos]>0.]
+			for i in range(len(dados_teste)):
 
-			if len(vizinhos) < k+1:
-				pass
-			else:
-				knn 		= vizinhos[argsort(similaridades[usuario, vizinhos])[-(k+1):-1]]
-				knn_sims 	= similaridades[usuario, knn]
-				notas 		= matriz_treino[knn, item].toarray()
+				print("\rCalculando predições: ", ((i+1)*100)//len(dados_teste), "%", end="")
 
-				num = dot(notas.T, knn_sims)
-				den = sum(abs(knn_sims))
+				usuario, item = dados_teste[i][0], dados_teste[i][1]
+				vizinhos = nonzero(matriz_treino[:, item])[0] 
+				vizinhos = vizinhos[similaridades[usuario, vizinhos]>0.]
 
-				if den > 0:
-					predito[i] = num/den 
+				if len(vizinhos) < k+1:
+					pass
+				else:
+					knn 		= vizinhos[argsort(similaridades[usuario, vizinhos])[-(k+1):-1]]
+					knn_sims 	= similaridades[usuario, knn]
+					notas 		= matriz_treino[knn, item].toarray()
 
-		print("\nCalculando média dos erros...")
-		escrever_estatisticas(arquivo_saida, k, calcular_mae(gabarito, predito), calcular_rmse(gabarito, predito), (count_nonzero(predito)/len(gabarito))*100)
+					num = dot(notas.T, knn_sims)
+					den = sum(abs(knn_sims))
+
+					if den > 0:
+						predito[i] = num/den 
+
+			print("\nCalculando média dos erros...")
+			escrever_estatisticas( (arquivo_saida + "_%d.csv") % (i,), k, calcular_mae(gabarito, predito), calcular_rmse(gabarito, predito), (count_nonzero(predito)/len(gabarito))*100)
 
 from random import shuffle
 
 if __name__ == "__main__":
 
-	dividir_base(u_data, divisoes = 5)
+	# dividir_base(u_data, divisoes = 5)
 
-	# classificar(func_similaridade=sc_dice, min_k=60, max_k=60,
-	# 			acrescimo=10, arquivo_saida="tests/saida.txt", limiar=1)
+	classificar(func_similaridade=sc_dice, min_k=10, max_k=100,
+				acrescimo=10, arquivo_saida="tests/sc_dice", divisoes=5, limiar=1)
 
-	# dados = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-	# divisoes = 5
-
-	# dados_divididos = []
-
-	# tamanho = len(dados)
-	# tamanho_parte = tamanho/ divisoes
-
-	# shuffle(dados)
-
-	# for i in range(divisoes):
-	# 	dados_divididos.append( dados[  int(i*tamanho_parte) : int((i+1)*tamanho_parte)] )
-
-	# print(dados_divididos)
 
 	# #########################################
 	# ############## Testes ###################
