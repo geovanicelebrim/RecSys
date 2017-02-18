69e32686bb4231a4c02fd1a748ccfcaef19b0c89
Geovani Celebrim <geovanicelebrim@gmail.com>
2017-02-18 21:54:08 -0200

    Adição da funcionalidade que permite analisar a acurácia da detecção de ruído da base.

--- RSVD/svd.py
+++ RSVD/svd.py
@@ -3,6 +3,7 @@ import numpy as np
 import os.path
 import time
 from cross_validation import dividir_base
+from random import randint
 
 dados_divididos = None
 
@@ -200,14 +201,9 @@ def load_model():
 
 	return (P,Q)
 
-def noise_rsvd(I, rating_train):
-
-	n_users, n_items = rating_train.shape
+def noise_rsvd(rating_train):
 
 	P,Q = load_model()
-	users,items = rating_train.nonzero()
-	best_test_rmse = float('inf')
-	best_test_mae = float('inf')
 
 	###### CALCULA OS ERROS #######
 	I2 = rating_train.copy()
@@ -242,6 +238,94 @@ def noise_rsvd(I, rating_train):
 	print(mae, rmse)
 	return new_rating
 
+def noise_detection(rating_train):
+	
+	index_original_noise, train = generate_noise(rating_train)
+
+	index_predicted_noise = np.full((rating_train.shape), -1, dtype=int)
+
+	P,Q = load_model()
+
+	###### CALCULA OS ERROS #######
+	rating = train
+	pred = prediction(P,Q)
+
+	for l in range(rating.shape[0]):
+		for c in range(rating.shape[1]):
+			if rating[l,c] != 0.0:
+				if abs(rating[l,c] - pred[l,c]) > 1:
+					index_predicted_noise[l,c] = 1
+				else:
+					index_predicted_noise[l,c] = 0
+
+	confusion(index_original_noise, index_predicted_noise)
+	
+
+def confusion(original, predicted):
+	true_noise = 0
+	false_noise = 0
+	true_non_noise = 0
+	false_non_noise = 0
+
+	for l in range(original.shape[0]):
+		for c in range(original.shape[1]):
+			if   (original[l,c] == 0) and (predicted[l,c] == 0):
+				true_non_noise += 1
+			elif (original[l,c] == 0) and (predicted[l,c] == 1):
+				false_noise += 1
+			elif (original[l,c] == 1) and (predicted[l,c] == 0):
+				false_non_noise += 1
+			elif (original[l,c] == 1) and (predicted[l,c] == 1):
+				true_noise += 1
+
+	print("Confusion Matrix:")
+	print("\t\tTrue\tFalse")
+	print("Noise:\t\t", true_noise, "\t", false_noise)
+	print("Non Noise:\t", true_non_noise, "\t", false_non_noise)
+
+	pass
+
+# if value = -1, this prediction is not valide
+# if value = 0, this prediction not contains noise
+# if value = 1, this prediction contains noise
+def generate_noise(rating_train, nNoise=1000):
+
+	index_noise = np.full((rating_train.shape), -1, dtype=int)
+	index_noise[rating_train > 0.0] = 0
+
+	valid_index = {}
+	count = 0;
+	for l in range(rating_train.shape[0]):
+		for c in range(rating_train.shape[1]):
+			if rating_train[l,c] > 0.0:
+				valid_index[count] = l,c
+				count += 1
+
+	if nNoise > len(valid_index):
+		print("Impossivel gerar essa quantidade de ruido.")
+		exit(1)
+
+	new_train = rating_train.copy()
+	for x in range(nNoise):
+		i = randint(0,len(valid_index) - 1)
+		while index_noise[valid_index[i][0], valid_index[i][1]] == 1:
+			i = randint(0,len(valid_index) - 1)
+		
+		index_noise[valid_index[i][0], valid_index[i][1]] = 1
+
+		if new_train[valid_index[i][0], valid_index[i][1]] < 3:
+			new_train[valid_index[i][0], valid_index[i][1]] = float(randint(3,5))
+		elif new_train[valid_index[i][0], valid_index[i][1]] > 3:
+			new_train[valid_index[i][0], valid_index[i][1]] = float(randint(1,3))
+		else:
+			gNoise = float(randint(1,5))
+			while gNoise == 3:
+				gNoise = float(randint(1,5))
+			new_train[valid_index[i][0], valid_index[i][1]] = gNoise
+
+	return (index_noise, new_train)
+
+
 """
 Melhores parametros:
 lamb = 0.11
@@ -251,13 +335,22 @@ lrate = 0.0066
 if __name__ == '__main__':
 
 	I, rating_train, rating_test = crete_dataset(0, 5)
+
+	noise_detection(rating_train)
+	# index, train = generate_noise(rating_train, 10)
+
+	# for l in range(index.shape[0]):
+	# 	for c in range(index.shape[1]):
+	# 		if index[l,c] == 1:
+	# 			print("Before: ", rating_train[l,c], "\tAfter: ", train[l,c])
+
 	# print("Resultado SVD sem tirar ruido:")
 	# rsvd(I, rating_train, rating_test)
-	print("------------------------------")
-	print("Operações para remover ruido:")
-	new_rating = noise_rsvd(I, rating_train)
+	# print("------------------------------")
+	# print("Operações para remover ruido:")
+	# new_rating = noise_rsvd(rating_train)
 
-	noise_rsvd(I, new_rating)
-	print("------------------------------")
-	print("Resultado SVD após tirar ruido:")
-	rsvd(I, new_rating, rating_test)
+	# noise_rsvd(new_rating)
+	# print("------------------------------")
+	# print("Resultado SVD após tirar ruido:")
+	# rsvd(I, new_rating, rating_test)
