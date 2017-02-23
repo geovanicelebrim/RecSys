a7d3e224026f315559e0f22ff01191675191e394
Geovani Celebrim <geovanicelebrim@gmail.com>
2017-02-23 02:53:24 -0300

    Resultados KNN

--- KNN/cross_validation.py
+++ KNN/cross_validation.py
@@ -1,62 +1,82 @@
 import os.path
 from random import shuffle
+from numpy import zeros
 
 path = "ml-100k/"
 
 """
-@brief Divide o conjunto de dados em partes de tamanhos iguais
+@brief Divide o conjunto de dados em subconjuntos de treino e teste
 
-@param arquivo Nome do arquivo de entrada
-@param divisoes Número de divisões do conjunto de dados
-@return dados_divididos Divisões do conjunto de dados
+@param indicesTreino Indices das notas de treino
+@param indicesTeste Indices das notas de teste
+
+@return matrizTreino Matriz de treino
+@return matrizTeste Matriz de teste
 
 """
-def dividir_base(arquivo, divisoes=5):
+def criarDataset(dados, usuarios, itens, indicesTreino, indicesTeste):
+
+	matrizTreino = zeros((len(usuarios), len(itens)))
+	matrizTeste = zeros((len(usuarios), len(itens)))
+
+	for indice in indicesTreino:
+		tokens = dados[indice].split()
+
+		usuario = int(tokens[0]) - 1
+		item 	= int(tokens[1]) - 1
+		nota 	= int(tokens[2])
+
+		matrizTreino[usuario, item] = nota
+
+	for indice in indicesTeste:
+		tokens = dados[indice].split()
+
+		usuario = int(tokens[0]) - 1
+		item 	= int(tokens[1]) - 1
+		nota 	= int(tokens[2])
+
+		matrizTeste[usuario, item] = nota
+
+	return matrizTreino, matrizTeste
+
+"""
+@brief Divide o conjunto de dados em treino e teste
+
+Dado o conjunto de dados, este é dividido em dois subconjuntos: treino e teste, 
+a uma porcentagem dada como parâmetro. 
+
+@param arquivo Nome do arquivo de entrada
+@param porcentagemTreino Porcentagem do conjunto de treino em relação ao conjunto de dados
+@return treino Dados do conjunto de treino
+@return teste Dados do conjunto de teste
+
+"""
+def treinoTesteSplit(arquivo, porcentagemTreino=.8):
 
 	dados = open(arquivo, 'r', encoding="utf-8").readlines()
 
-	tamanho = len(dados)
+	conjuntoDeTreino = path + "treino.txt"
+	conjuntoDeTeste = path + "teste.txt"
 
-	if (tamanho % divisoes) != 0:
-		print("O número de divisões não gera partes de tamanhos iguais!")
-		return None
+	if not os.path.exists(conjuntoDeTreino) or not os.path.exists(conjuntoDeTeste):
+		tamanho = len(dados)
+		posicao = int(tamanho*porcentagemTreino)
+		for i in range(10):
+			shuffle(dados)
+		treino, teste = dados[:posicao], dados[posicao:]
+		
+		with open(conjuntoDeTreino, 'w') as f:
+			for linha in treino:
+				f.write(linha)
 
-	tamanho_parte = tamanho/ divisoes
-
-	nome_base = path + "base_%s.txt"
-
-	dados_divididos = []
-
-	arquivos_existem = True
-
-	for i in range(divisoes):
-
-		nome = nome_base % (i+1,)
-
-		if os.path.exists(nome):
-			f = open(nome_base % (i+1,), 'r')
-
-			arquivos_existem = arquivos_existem and (len(f.readlines()) == tamanho_parte)
-		else:
-			arquivos_existem = False
-
-	if not arquivos_existem:
-
-		shuffle(dados)
-
-		for i in range(divisoes):
-			dados_divididos.append( dados[ int(i*tamanho_parte) :  int((i+1)*tamanho_parte)] )
-
-			with open(nome_base % (i+1,), 'w') as f:
-				for linha in dados_divididos[i]:
-					f.write(linha)
-
+		with open(conjuntoDeTeste, 'w') as f:
+			for linha in teste:
+				f.write(linha)
 	else:
-		for i in range(divisoes):
-			with open(nome_base % (i+1,), 'r') as f:
-				dados_divididos.append(f.readlines())
+		treino = open(conjuntoDeTreino)
+		teste = open(conjuntoDeTeste)
 
-	return dados_divididos
+	return treino, teste
 
 """
 @brief Calcula a média absoluta do erro
@@ -68,7 +88,7 @@ Dados o gabarito e predito, calcula a média absoluta dos erros
 @return mae Média absoluta do erro
 
 """
-def calcular_mae(gabarito, predito):
+def calcularMAE(gabarito, predito):
 	mae = 0
 	t = 0
 	for i in range(len(predito)):
@@ -78,7 +98,8 @@ def calcular_mae(gabarito, predito):
 	mae /= t
 	return mae
 
-def calcular_rmse(gabarito, predito):
+
+def calcularRMSE(gabarito, predito):
 	rmse = 0
 	t = 0
 	for i in range(len(predito)):
@@ -87,3 +108,34 @@ def calcular_rmse(gabarito, predito):
 			t = t + 1
 	rmse = (rmse/t)**.5
 	return rmse
+
+
+def calcularPrecisionRecall(gabarito, predito):
+	TP = 0
+	TN = 0
+	FP = 0
+	FN = 0
+
+	for i in range(gabarito.shape[0]):
+		for j in range(gabarito.shape[1]):
+			if gabarito[i, j] == 0 and predito[i, j] == 0:
+				TN = TN + 1
+			elif gabarito[i, j] == 0 and predito[i, j] == 1:
+				FP = FP + 1
+			elif gabarito[i, j] == 1 and predito[i, j] == 0:
+				FN = FN + 1
+			elif gabarito[i, j] == 1 and predito[i, j] == 1:
+				TP = TP + 1
+
+	precision = TP / (TP + FP)
+	recall = TP / (TP + FN)
+
+	return (precision, recall)
+
+
+def calcularF1Score(gabarito, predito):
+	precision, recall = calcularPrecisionRecall(gabarito, predito)
+
+	f1 = (2 * precision * recall) / (precision + recall)
+
+	return (precision, recall, f1)
--- KNN/similarities.py
+++ KNN/similarities.py
@@ -1,5 +1,5 @@
-from numpy import full, zeros, dot, sum as soma, mean, intersect1d, nonzero, count_nonzero
-from sklearn.metrics.pairwise import cosine_similarity
+from numpy import full, zeros, sum as soma, mean, intersect1d, nonzero, count_nonzero
+from sklearn.metrics.pairwise import cosine_similarity as cosine
 
 """
 @brief Calcula média da matriz esparsa
@@ -7,13 +7,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 @return media Média da matriz
 
 """
-def media_matriz(matriz):
-	media = 0
-	for i in range(matriz.shape[0]):
-		for j in range(matriz.shape[1]):
-			media += matriz[i, j]
-	media /= matriz.nnz
-	return media
+def mediaMatriz(matriz):
+	return mean(matriz[matriz>0])
 
 """
 @brief Calcula a média do usuário usando shrinkage
@@ -26,7 +21,7 @@ Dadas as notas do usuário e a média global, calcula a média do usuário propo
 @return Média do usuário
 
 """
-def media_usuario(notas, mu, shrinkage=10):
+def mediaUsuario(notas, mu, shrinkage=10):
 	ru = count_nonzero(notas)
 	b = sum(notas) / ru
 	dem = shrinkage + ru
@@ -42,14 +37,14 @@ A média global é adiciona aos casos em que não há avaliação do usuário pa
 @return Similaridade calculada
 """
 def cosseno(matriz):
-	mu = media_matriz(matriz)
-	copy = full(matriz.shape, mu) 
-	lines, cols = matriz.nonzero()
+	mu = mediaMatriz(matriz)
+	copia = full(matriz.shape, mu) 
+	linhas, colunas = matriz.nonzero() 
 
-	for l, c in zip(lines, cols):
-		copy[l, c] = matriz[l, c]
+	for l, c in zip(linhas, colunas):
+		copia[l, c] = matriz[l, c]
 
-	return cosine_similarity(copy)
+	return cosine(copia)
 
 
 """
@@ -62,7 +57,7 @@ Dados dois usuários, u e v, o cálculo é feito considerando apenas os itens av
 @return similaridades Matriz de similaridade usuário-usuário
 
 """
-def cosseno_intersecao(matriz):
+def cossenoIntersecao(matriz):
 	similaridades = zeros((matriz.shape[0], matriz.shape[0]))
 	
 	for u in range(matriz.shape[0]):
@@ -73,19 +68,19 @@ def cosseno_intersecao(matriz):
 
 			cos = 0.
 
-			iu = nonzero(matriz[u,:])[1] # índices dos itens avaliados pelo usúario u
-			iv = nonzero(matriz[v,:])[1] # índices dos itens avaliados pelo usuário v
+			iu = nonzero(matriz[u,:]) # índices dos itens avaliados pelo usúario u
+			iv = nonzero(matriz[v,:]) # índices dos itens avaliados pelo usuário v
 			
 			intersecao = intersect1d(iu, iv) # índices das itens avaliados por u e v
 
-			nu = matriz[u, iu].toarray()[0] # notas de u
-			nv = matriz[v, iv].toarray()[0] # notas de v	
+			nu = matriz[u, iu] # notas de u
+			nv = matriz[v, iv] # notas de v	
 
 			den = (soma(nu**2) * soma(nv**2))**.5 # considera todas as notas de u e todas as notas de v
 
 			if len(intersecao) > 10:
-				nu = matriz[u, intersecao].toarray()[0] # notas dos itens avaliados por u e v
-				nv = matriz[v, intersecao].toarray()[0] # notas dos itens avaliados por v e u
+				nu = matriz[u, intersecao] # notas dos itens avaliados por u e v
+				nv = matriz[v, intersecao] # notas dos itens avaliados por v e u
 
 				num = soma(nu * nv)				
 
@@ -106,7 +101,7 @@ def cosseno_intersecao(matriz):
 def pearson(matriz):
 	similaridades = zeros((matriz.shape[0], matriz.shape[0]))
 
-	mg = media_matriz(matriz)
+	mg = mediaMatriz(matriz)
 
 	for u in range(matriz.shape[0]):
 
@@ -116,11 +111,11 @@ def pearson(matriz):
 
 			pearson = 0.
 
-			iu = nonzero(matriz[u, :])[1] # índice dos itens avaliados por u
-			iv = nonzero(matriz[v, :])[1] # índice dos itens avaliados por v
+			iu = nonzero(matriz[u, :]) # índice dos itens avaliados por u
+			iv = nonzero(matriz[v, :]) # índice dos itens avaliados por v
 
-			nu = matriz[u, :].toarray()[0] # notas de u
-			nv = matriz[v, :].toarray()[0] # notas de v	
+			nu = matriz[u, :] # notas de u
+			nv = matriz[v, :] # notas de v	
 
 			intersecao = intersect1d(iu, iv) # índice dos itens em comum
 
@@ -128,11 +123,8 @@ def pearson(matriz):
 				nu = nu[intersecao] # notas de u de itens em comum com v
 				nv = nv[intersecao] # notas de v de itens em comum com u
 
-				# mu = soma(nu)/len(nu) # média de u
-				# mv = soma(nv)/len(nv) # média de v
-
-				mu = media_usuario(nu, mg)
-				mv = media_usuario(nv, mg)
+				mu = mediaUsuario(nu, mg)
+				mv = mediaUsuario(nv, mg)
 
 				num = soma((nu - mu) * (nv - mv))
 				den = (soma((nu - mu)**2) * soma((nv - mv)**2))**.5
@@ -146,42 +138,88 @@ def pearson(matriz):
 	return similaridades
 
 """
-@brief Calcula a similaridade SC-Dice entre os usuários 
+@brief Calcula o inverso da diferença quadrática
+
+Dada a matriz usuário-item, calcula a similaridade entre dois ussuários
+calculando a o inverso da média dos quadrados da diferença entre as notas da interseção.
+
 @param matriz Matriz usuário-item
-@param limiar Diferença entre notas máxima a ser considerada
-@return similaridades Matriz de similaridade usuário-usuário
+@return similaridades Matriz de similaridades
 
 """
-def sc_dice(matriz, limiar=1):
+def meanSquaredDifference(matriz):
 
 	similaridades = zeros((matriz.shape[0], matriz.shape[0]))
 
 	for u in range(matriz.shape[0]):
 
-		# print("\r", u+1, " de ", matriz.shape[0], end="")
 		print("\rCalculando similaridades: ", ((u+1)*100)//matriz.shape[0], "%", end="")
 
-		for v in range(u + 1):
+		for v in range(u+1):
 
-			cd = 0.
+			msd = 0.
 
-			iu = nonzero(matriz[u, :])[1] # índice dos itens avaliados por u
-			iv = nonzero(matriz[v, :])[1] # índice dos itens avaliados por v	
+			iu = nonzero(matriz[u, :]) # índice dos itens avaliados por u
+			iv = nonzero(matriz[v, :]) # índice dos itens avaliados por v
 
-			intersecao = intersect1d(iu, iv) # índices das itens avaliados por u e v
+			nu = matriz[u, :] # notas de u
+			nv = matriz[v, :] # notas de v
 
-			if len(intersecao) > 10:
+			intersecao = intersect1d(iu, iv) # índices da interseção
 
-				nu = matriz[u, intersecao].toarray()[0] # notas dos itens avaliados por u e v
-				nv = matriz[v, intersecao].toarray()[0] # notas dos itens avaliados por v e u
+			if len(intersecao) > 0:
+				nu = nu[intersecao] # notas de u de itens em comum com v
+				nv = nv[intersecao] # notas de v de itens em comum com u
 
-				num = len([i for i in range(len(intersecao)) if abs(nu[i] - nv[i]) <= limiar])
+				num = len(intersecao)
+				den = soma((nu - nv)**2)
 
-				if len(intersecao) > 0:
-					cd = (2.0 * num) / (nu.shape[0] + nv.shape[0])
+				if den > 0.:
+					msd = num/den
 
-				similaridades[u, v] = cd
-				similaridades[v, u] = cd
-		
+				similaridades[u, v] = msd
+				similaridades[v, u] = msd
+
 	return similaridades
 
+"""
+@brief Calcula a similaridade Silva-Oliveira
+
+Dada a matriz de usuário-item, calcula a similaridade entre dois usuários 
+considerando a porcentagem de notas da interseção cuja diferença absoluta seja menor ou igual a um limiar.
+
+@param matriz Matriz usuário-item
+@param limiar Limiar da diferença absoluta das notas. Default 1. 
+@return similaridades Matriz de similaridades.
+
+"""
+def silvaOliveira(matriz, limiar=1):
+
+	similaridades = zeros((matriz.shape[0], matriz.shape[0]))
+
+	for u in range(matriz.shape[0]):
+
+		print("\rCalculando similaridades: ", ((u+1)*100)//matriz.shape[0], "%", end="")
+		
+		for v in range(u+1):
+
+			rd = 0.
+
+			iu = nonzero(matriz[u,:]) # índices dos itens avaliados pelo usúario u
+			iv = nonzero(matriz[v,:]) # índices dos itens avaliados pelo usuário v
+			
+			intersecao = intersect1d(iu, iv) # índices das itens avaliados por u e v
+
+			nu = matriz[u, intersecao] # notas dos itens avaliados por u e v
+			nv = matriz[v, intersecao] # notas dos itens avaliados por v e u
+
+			num = len([i for i in range(len(intersecao)) if abs(nu[i] - nv[i]) <= limiar]) 
+
+			if len(intersecao) > 10:
+				rd = num/len(intersecao)
+
+			similaridades[u, v] = rd
+			similaridades[v, u] = rd
+
+	return similaridades
+	
